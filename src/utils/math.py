5eb928761b8fae6eef9e04fa5b13c9f58e702609
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-10-18 00:58:49 +0800

    docs(math): Added documentation comments to the mean squared error loss calculation function

--- src/utils/math.py
+++ src/utils/math.py
@@ -6,7 +6,7 @@ from torch import matmul
 reshape = lambda x, *args, **kwargs: x.reshape(*args, **kwargs)
 
 def linreg(X: torch.Tensor, w: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-    """_summary_
+    """_summary_:
         Linear regression, 计算结果  
     
     Args:
@@ -20,4 +20,14 @@ def linreg(X: torch.Tensor, w: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     return matmul(X, w) + b
 
 def squared_loss(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """_summary_:
+        Squared loss, 平方损失函数  
+
+    Args:
+        y_hat (torch.Tensor): _description_ 预测值  
+        y (torch.Tensor): _description_ 真实值  
+
+    Returns:
+        torch.Tensor: _description_ 损失值  
+    """
     return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2

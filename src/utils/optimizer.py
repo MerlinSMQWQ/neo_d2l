8c106a4cb120f6a0fb987cafb1c61220d392861e
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-10-19 00:36:06 +0800

    docs(math, optimizer, quick_gen): Optimized the comment documentation for easier reading.

--- src/utils/optimizer.py
+++ src/utils/optimizer.py
@@ -6,9 +6,9 @@ def sgd(params: Iterable[torch.Tensor], lr: float, batch_size: int):
         SGD, 随机梯度下降
 
     Args:
-        params (Iterable[torch.Tensor]): _description_ 参数  
-        lr (float): _description_ 学习率  
-        batch_size (int): _description_ 批量大小  
+        - params (Iterable[torch.Tensor]): _description_ 参数  
+        - lr (float): _description_ 学习率  
+        - batch_size (int): _description_ 批量大小  
     """
     with torch.no_grad():
         for param in params:

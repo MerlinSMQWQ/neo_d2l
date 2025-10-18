6647038d93f08573c3bf1e457512f7f57df8fb01
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-10-19 00:10:47 +0800

    chore(init): Changes were made to the __init__.py file, exposing interfaces through this file.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
 __version__ = '0.0.1'
+
+from .src.utils.display import use_TKAgg_display, set_figsize, set_axes, plot
+from .src.utils.math import linreg, squared_loss
+from .src.utils.quick_gen import synthetic_data
+from .src.utils.optimizer import sgd
+from .src.utils.timer import Timer
+
+__all__ = [
+    'use_TKAgg_display',
+    'set_figsize',
+    'set_axes',
+    'plot',
+    'linreg',
+    'squared_loss',
+    'synthetic_data',
+    'sgd',
+    'Timer'
+]
